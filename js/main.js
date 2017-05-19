c345f52228b138d7ddeac762d6c943d145a9d3ba
YouSeeStefan <stefan.stekelenburg@gmail.com>
2017-05-19 14:51:10 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import AlertNotification from './alertnotification';
+import Notification from './notification';
 
-const notificationBar = new AlertNotification(".notification-bar");
+const notificationBar = new Notification(".notification-bar");
 let countClick = 0;
 
 notificationBar.showMessage("Welkom op deze pagina");
@@ -9,4 +9,15 @@ document.addEventListener("click",
                           () => {
                               countClick++;
                               notificationBar.showMessage(`je hebt ${countClick}x geklikt`);
+                          });
+
+
+const notificationBar2 = new Notification(".notification-bar2");
+let countClick2 = 0;
+notificationBar2.showMessage("Welkom in de hell :)");
+
+document.addEventListener("click",
+                          () => {
+                              countClick2++;
+                              notificationBar2.showMessage(`je hebt ${countClick2}x geklikt`);
                           });
